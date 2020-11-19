96403ddb1be10d630b95bb1654f9b37fa42aaf9a
Kenan Casey <kenancasey@gmail.com>
2020-11-19 14:12:38 -0600

    Convert from sheet.best to native google sheet api

--- covid-dashboard/main.js
+++ covid-dashboard/main.js
@@ -1,4 +1,7 @@
-$(function(){
+const totalStudents = 1132;
+const totalFacultyStaff = 300;
+
+$(function () {
 
     getDates();
     getData();
@@ -13,7 +16,7 @@ $(function(){
         $(this).popover('hide');
     });
 
-    $("#dateSelect").change(function() {
+    $("#dateSelect").change(function () {
         getData();
     });
 
@@ -23,7 +26,7 @@ async function getDates() {
     let response = await fetch("https://sheet.best/api/sheets/bf9fbbc3-aac1-48f4-98f2-7715cbb6d295");
     let data = await response.json();
     let values = Object.values(data);
-    for (i=data.length-1; i>=0; i--){
+    for (i = data.length - 1; i >= 0; i--) {
         if (values[i].id == null || values[i].date == null || values[i].activeCasesStudent == null || values[i].activeCasesFacultyStaff == null || values[i].isolated == null || values[i].quarantined == null || values[i].riskLevel == null) {
             continue;
         }
@@ -33,15 +36,44 @@ async function getDates() {
     };
 };
 
-async function getData(){
-    let response = await fetch("https://sheet.best/api/sheets/bf9fbbc3-aac1-48f4-98f2-7715cbb6d295");
-    let data = await response.json();
-    let index = getIndex(data);
-    let current = data[index];
-    let values = Object.values(current);
+var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/1XJBCL5aTxpPcfBQhfyDqqAKglcZcnN-srGsZc-P2Sq0/1/public/full?alt=json';
+
+async function getData() {
+    // let response = await fetch("https://sheet.best/api/sheets/bf9fbbc3-aac1-48f4-98f2-7715cbb6d295");
+    // let data = await response.json();
+    // let index = getIndex(data);
+    // let current = data[index];
+    // let values = Object.values(current);
+
+
+    let response = await fetch(sheetUrl);
+    let sheetData = await response.json();
+    let entry = sheetData.feed.entry;
+    console.log(entry);
+    
+    let data = [];
+    let numCols = 7;
+
+    for(let i = numCols; i < entry.length; i+=numCols){
+        data.push( {
+            id: entry[i].content.$t,
+            date: entry[i+1].content.$t,
+            activeCasesStudent: parseInt(entry[i + 2].content.$t),
+            activeCasesFacultyStaff: parseInt(entry[i + 3].content.$t),
+            isolated: parseInt(entry[i + 4].content.$t),
+            quarantined: parseInt(entry[i+5].content.$t),
+            riskLevel : entry[i + 6].content.$t
+         } );  
+    }
+
+    console.log(data);
+    
+    let values = Object.values(data);
     getValues(values);
     drawCharts(data);
 };
+
+
 
 function getValues(values) {
     $("#date").html(values[1]);
@@ -55,9 +87,9 @@ function getValues(values) {
 
 function getIndex(data) {
     let date = $("#dateSelect").val();
-    let index = data.length-1;
+    let index = data.length - 1;
 
-    for (i=data.length-1; i>=0; i--){
+    for (i = data.length - 1; i >= 0; i--) {
         if (date == data[i].date) index = (data[i].id - 1);
     };
 
@@ -65,25 +97,25 @@ function getIndex(data) {
 };
 
 function setColor(color) {
-    if (color == "Green"){
+    if (color == "Green") {
         $(".bilboard h2").addClass("green");
     }
-    else if (color == "Yellow"){
+    else if (color == "Yellow") {
         $(".bilboard h2").addClass("yellow");
     }
-    else if (color == "Orange"){
+    else if (color == "Orange") {
         $(".bilboard h2").addClass("orange");
     }
-    else if (color == "Red"){
+    else if (color == "Red") {
         $(".bilboard h2").addClass("red");
     }
 };
 
 function drawCharts(input) {
 
-    let currentActiveStudentCases = input[input.length-1].activeCasesStudent;
-    let currentActiveFacultyCases = input[input.length-1].activeCasesFacultyStaff;
-    let currentTotalQuarantinedIsolated = +input[input.length-1].quarantined + +input[input.length-1].isolated;
+    let currentActiveStudentCases = input[input.length - 1].activeCasesStudent;
+    let currentActiveFacultyCases = input[input.length - 1].activeCasesFacultyStaff;
+    let currentTotalQuarantinedIsolated = +input[input.length - 1].quarantined + +input[input.length - 1].isolated;
     let activeStudentCasesList = [];
     let activeFacultyCasesList = [];
     let quarantinedIsolatedList = [];
@@ -100,128 +132,128 @@ function drawCharts(input) {
 
     var studentsAffectedDoughnut = document.getElementById("studentsAffectedDoughnut");
     var myDoughnut1 = new Chart(studentsAffectedDoughnut, {
-    type: 'pie',
-    data: {
-        labels: ['Current Student Active Cases', 'Total Students'],
-        datasets: [{
-        data: [currentActiveStudentCases, 1132],
-        backgroundColor: [
-            '#ffd301',
-            '#815558'
-        ],
-        borderWidth: 0
-        }]
-    }
+        type: 'pie',
+        data: {
+            labels: ['Current Student Active Cases', 'Total Students'],
+            datasets: [{
+                data: [currentActiveStudentCases, totalStudents],
+                backgroundColor: [
+                    '#ffd301',
+                    '#815558'
+                ],
+                borderWidth: 0
+            }]
+        }
     });
 
     var facultyAffectedDoughnut = document.getElementById("facultyAffectedDoughnut");
     var myDoughnut2 = new Chart(facultyAffectedDoughnut, {
-    type: 'pie',
-    data: {
-        labels: ['Current Faculty/Staff Active Cases', 'Total Faculty/Staff'],
-        datasets: [{
-        data: [currentActiveFacultyCases, 300],
-        backgroundColor: [
-            '#ffd301',
-            '#815558'
-        ],
-        borderWidth: 0
-        }]
-    }
+        type: 'pie',
+        data: {
+            labels: ['Current Faculty/Staff Active Cases', 'Total Faculty/Staff'],
+            datasets: [{
+                data: [currentActiveFacultyCases, totalFacultyStaff],
+                backgroundColor: [
+                    '#ffd301',
+                    '#815558'
+                ],
+                borderWidth: 0
+            }]
+        }
     });
 
     var quarantinedIsolatedDoughnut = document.getElementById("quarantinedIsolatedDoughnut");
     var myDoughnut3 = new Chart(quarantinedIsolatedDoughnut, {
-    type: 'doughnut',
-    data: {
-        labels: ['Total in Quarantine or Isolation', 'Total On Campus'],
-        datasets: [{
-        data: [currentTotalQuarantinedIsolated, 1132 + 300],
-        backgroundColor: [
-            '#ffd301',
-            '#815558'
-        ],
-        borderWidth: 0
-        }]
-    }
+        type: 'doughnut',
+        data: {
+            labels: ['Total in Quarantine or Isolation', 'Total On Campus'],
+            datasets: [{
+                data: [currentTotalQuarantinedIsolated, 1132 + 300],
+                backgroundColor: [
+                    '#ffd301',
+                    '#815558'
+                ],
+                borderWidth: 0
+            }]
+        }
     });
 
     var activeStudentCasesChart = document.getElementById('activeStudentCasesChart');
     var myChart1 = new Chart(activeStudentCasesChart, {
-    type: 'line',
-    data: {
-        labels: datesList,
-        datasets: [{
-            label: 'Number of Active Student Cases',
-            data: activeStudentCasesList,
-            pointBackgroundColor: '#815558',
-            pointBorderColor: 'rgb(128,0,0)',
-            backgroundColor: '#815558',
-            borderColor: 'rgb(128,0,0)',
-            borderWidth: 3
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+        type: 'line',
+        data: {
+            labels: datesList,
+            datasets: [{
+                label: 'Number of Active Student Cases',
+                data: activeStudentCasesList,
+                pointBackgroundColor: '#815558',
+                pointBorderColor: 'rgb(128,0,0)',
+                backgroundColor: '#815558',
+                borderColor: 'rgb(128,0,0)',
+                borderWidth: 3
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
     });
 
     var activeFacultyCasesChart = document.getElementById('activeFacultyCasesChart');
     var myChart2 = new Chart(activeFacultyCasesChart, {
-    type: 'line',
-    data: {
-        labels: datesList,
-        datasets: [{
-            label: 'Number of Active Faculty Cases',
-            data: activeFacultyCasesList,
-            pointBackgroundColor: '#815558',
-            pointBorderColor: 'rgb(128,0,0)',
-            backgroundColor: '#815558',
-            borderColor: 'rgb(128,0,0)',
-            borderWidth: 3
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+        type: 'line',
+        data: {
+            labels: datesList,
+            datasets: [{
+                label: 'Number of Active Faculty Cases',
+                data: activeFacultyCasesList,
+                pointBackgroundColor: '#815558',
+                pointBorderColor: 'rgb(128,0,0)',
+                backgroundColor: '#815558',
+                borderColor: 'rgb(128,0,0)',
+                borderWidth: 3
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
     });
 
     var quarantinedIsolatedChart = document.getElementById('quarantinedIsolatedChart');
     var myChart3 = new Chart(quarantinedIsolatedChart, {
-    type: 'line',
-    data: {
-        labels: datesList,
-        datasets: [{
-            label: 'Total in Quarantine or Isolation',
-            data: quarantinedIsolatedList,
-            pointBackgroundColor: '#815558',
-            pointBorderColor: 'rgb(128,0,0)',
-            backgroundColor: '#815558',
-            borderColor: 'rgb(128,0,0)',
-            borderWidth: 3
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+        type: 'line',
+        data: {
+            labels: datesList,
+            datasets: [{
+                label: 'Total in Quarantine or Isolation',
+                data: quarantinedIsolatedList,
+                pointBackgroundColor: '#815558',
+                pointBorderColor: 'rgb(128,0,0)',
+                backgroundColor: '#815558',
+                borderColor: 'rgb(128,0,0)',
+                borderWidth: 3
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
     });
 };
 
